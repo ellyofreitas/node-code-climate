2c694496d3e479016105666ac7034c53dfdcab6c
Ellyo Freitas <ellyofreitas13@gmail.com>
2021-08-01 18:17:54 -0300

    refactor and add div

--- calc.js
+++ calc.js
@@ -1,6 +1,7 @@
 const sum = require('./sum');
 const sub = require('./sub');
 const mut = require('./mut');
+const div = require('./div');
 const assert = require('./assert');
 
 module.exports = (op, a, b) => {
@@ -8,9 +9,18 @@ module.exports = (op, a, b) => {
   assert(!!a, 'a must be required');
   assert(!!b, 'b must be required');
 
-  if (op === '+') { return sum(a, b); }
-  if (op === '-') { return sub(a, b); }
-  if (op === '*') { return mut(a, b); }
+  if (op === '+') {
+    return sum(a, b);
+  }
+  if (op === '-') {
+    return sub(a, b);
+  }
+  if (op === '*') {
+    return mut(a, b);
+  }
+  if (op === '/') {
+    return div(a, b);
+  }
 
   throw new Error('op not implemented')
 }
